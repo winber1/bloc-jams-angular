74fcef167c13ca2259c367ccf0f8ef9ed70375f2
winber1 <winber@cox.net>
2015-11-08 21:40:36 -0500

    checkpoint-02 - complete: Angular - Routing and States

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,25 @@
-var blocJams = angular.module('blocJams', []);
+var blocJams = angular.module('blocJams', [ui.router]);
+
+// $stateProvider - config app's paths
+blocJams.config(function($stateProvider, $locationProvider) 
+{
+    $locationProvider.html5Mode
+    ({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $stateProvider
+        .state('album', 
+        {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '/templates/album.html'
+        })
+        .state('test', 
+        {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '/templates/album.html'
+        });
+});

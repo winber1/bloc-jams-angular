962a20531325bb5fbe7e2068d690471592682238
winber1 <winber@cox.net>
2015-11-16 19:26:30 -0500

    checkpoint 04 Angular complete - Controllers

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,23 @@
 var blocJams = angular.module('blocJams', ['ui.router']);
 
+    // albums
+    var albumPicasso = 
+    {
+        name: 'The Colors',
+        artist: 'Pablo Picasso',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songCount: 5
+    };
+ 
+    // Another Example Album
+    var albumMarconi = 
+    {
+        name: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songCount: 5
+    };
+
 // $stateProvider 
 // $locationProvider- config app's paths
 blocJams.config(function($stateProvider, $locationProvider) 
@@ -17,19 +35,58 @@ blocJams.config(function($stateProvider, $locationProvider)
         .state('album', 
         {
             url: '/album',
-            controller: 'Album.controller',
+            controller: 'AlbumController',
             templateUrl: '/templates/album.html'
         })
         .state('collection', 
         {
             url: '/collection',
-            controller: 'Collection.controller',
+            controller: 'CollectionController',
             templateUrl: '/templates/collection.html'
         })
         .state('landing', 
         {
             url: '/landing',
-            controller: 'Landing.controller',
+            controller: 'LandingController',
             templateUrl: '/templates/landing.html'
         });
 });
+
+
+blocJams.controller('AlbumController', ['$scope', function($scope) 
+{
+    //console.log('AlbumController loaded');
+    $scope.landingText = "Turn the music up!";
+  
+    $scope.addExclamation = function() 
+    {
+        $scope.landingText += "!";
+    };
+}]);
+
+
+blocJams.controller('CollectionController', ['$scope', function($scope) 
+{
+
+    
+    $scope.albumCollection = angular.copy([albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
+            albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
+            albumPicasso, albumMarconi, albumPicasso, albumMarconi]);
+        
+        /*
+        [albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
+            albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
+            albumPicasso, albumMarconi, albumPicasso, albumMarconi];
+    */
+}]);
+
+
+blocJams.controller('LandingController', ['$scope', function($scope) 
+{
+    $scope.landingText = "Turn the music up!";
+  
+    $scope.addExclamation = function() 
+    {
+        $scope.landingText += "!";
+    };
+}]);

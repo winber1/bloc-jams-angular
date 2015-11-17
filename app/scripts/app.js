b79b91c0f8bcfe41921fc9de48677e0736ad0800
winber1 <winber@cox.net>
2015-11-16 20:25:30 -0500

    assignment 04 Angular complete: Controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,5 @@
 var blocJams = angular.module('blocJams', ['ui.router']);
 
-    // albums
-    var albumPicasso = 
-    {
-        name: 'The Colors',
-        artist: 'Pablo Picasso',
-        albumArtUrl: 'assets/images/album_covers/01.png',
-        songCount: 5
-    };
- 
-    // Another Example Album
-    var albumMarconi = 
-    {
-        name: 'The Telephone',
-        artist: 'Guglielmo Marconi',
-        albumArtUrl: 'assets/images/album_covers/20.png',
-        songCount: 5
-    };
-
 // $stateProvider 
 // $locationProvider- config app's paths
 blocJams.config(function($stateProvider, $locationProvider) 
@@ -55,29 +37,20 @@ blocJams.config(function($stateProvider, $locationProvider)
 
 blocJams.controller('AlbumController', ['$scope', function($scope) 
 {
-    //console.log('AlbumController loaded');
-    $scope.landingText = "Turn the music up!";
+    $scope.album = angular.copy(albumPicasso);
+
   
-    $scope.addExclamation = function() 
-    {
-        $scope.landingText += "!";
-    };
+    
 }]);
 
 
 blocJams.controller('CollectionController', ['$scope', function($scope) 
-{
-
-    
-    $scope.albumCollection = angular.copy([albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
-            albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
-            albumPicasso, albumMarconi, albumPicasso, albumMarconi]);
-        
-        /*
-        [albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
-            albumPicasso, albumMarconi, albumPicasso, albumMarconi, 
-            albumPicasso, albumMarconi, albumPicasso, albumMarconi];
-    */
+{  
+    $scope.albumCollection = 
+        angular.copy([albumPicasso, albumMarconi, albumPicasso,
+                      albumMarconi, albumPicasso, albumMarconi, 
+                      albumPicasso, albumMarconi, albumPicasso,
+                      albumMarconi, albumPicasso, albumMarconi]);
 }]);
 
 

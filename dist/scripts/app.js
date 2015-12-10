9099b08a91620dd7817793c28069e39651a596be
winber1 <winber@cox.net>
2015-12-09 21:01:42 -0500

    changes for AngularLesson-updated-checkpoint5-Services

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -35,13 +35,57 @@ blocJams.config(function($stateProvider, $locationProvider)
 });
 
 
-// ------ Services --------------------------------------
-blocJams.service('SongPlayer', function() {
+// ------ Factories, Services -----------------------------
+blocJams.factory('Fixtures', function() 
+{
+    // song data
+ 
+        var Fixtures = {};
+        
+        var albumPicasso = 
+     {
+     name: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: 161.71, audioUrl: '/assets/music/blue' },
+         { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+         { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+         { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+         { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+      ]
+      };
+ 
+     var albumMarconi = {
+         name: 'The Telephone',
+         artist: 'Guglielmo Marconi',
+         label: 'EM',
+         year: '1909',
+         albumArtUrl: '/assets/images/album_covers/20.png',
+         songs: [
+             { name: 'Hello, Operator?', length: '1:01' },
+             { name: 'Ring, ring, ring', length: '5:01' },
+             { name: 'Fits in your pocket', length: '3:21' },
+             { name: 'Can you hear me now?', length: '3:14' },
+             { name: 'Wrong phone number', length: '2:15' }
+         ]
+     };
+        
+        Fixtures.getAlbum = function() 
+        { return albumPicasso; };
+        
+        return Fixtures;
+});
+
+blocJams.service('SongPlayer', function() 
+{
     
     //test function
     this.square = function(a) { return a*a };
- 
-    // play song --------------------------------------------
+
+    // play song -------------------------------------------
     this.setSong = function(album, songNumber) 
     {
  
@@ -68,11 +112,12 @@ blocJams.service('SongPlayer', function() {
 });
 
 // ------ Controllers --------------------------------------
-blocJams.controller('AlbumController', 
-                    function($scope, SongPlayer) 
+blocJams.controller('AlbumController', ['$scope', 'SongPlayer', 'Fixtures',
+                    function($scope, SongPlayer, Fixtures) 
 {
     console.log('AlbumContorller loaded');
-    $scope.album = angular.copy(albumPicasso);
+    $scope.album = Fixtures.getAlbum();
+    //$scope.album = angular.copy(albumPicasso);
     $scope.songNumber = 3;
     
     // test service
@@ -84,7 +129,7 @@ blocJams.controller('AlbumController',
     $scope.playSong = function () 
     {  SongPlayer.setSong($scope.album, $scope.songNumber); }
     
-});
+}]);
 
 
 blocJams.controller('CollectionController', ['$scope', function($scope) 
